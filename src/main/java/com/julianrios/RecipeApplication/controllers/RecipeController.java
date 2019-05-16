27fdb1d10b9d44d84e24566aea4b849abb0481a4
Julian Rios <julianrios.dev@gmail.com>
2019-05-16 12:37:59 -0400

    added service, controller, repository for Recipe

---
+++ src/main/java/com/julianrios/RecipeApplication/controllers/RecipeController.java
@@ -0,0 +1,8 @@
+package com.julianrios.RecipeApplication.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RecipeController {
+
+}
---
+++ src/main/java/com/julianrios/RecipeApplication/services/RecipeService.java
@@ -0,0 +1,7 @@
+package com.julianrios.RecipeApplication.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class RecipeService {
+}
