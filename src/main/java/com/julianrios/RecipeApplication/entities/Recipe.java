c9e415f691ef97bb6d0fbe3adbc3c4416ce05553
Julian Rios <julianrios.dev@gmail.com>
2019-05-16 12:34:07 -0400

    created necessary fields to connect spoonacular api

--- src/main/java/com/julianrios/RecipeApplication/entities/Recipe.java
+++ src/main/java/com/julianrios/RecipeApplication/entities/Recipe.java
@@ -1,5 +1,33 @@
 package com.julianrios.RecipeApplication.entities;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
 public class Recipe {
-    // TODO speak with Zomato API & Recipes Spoonacular API
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String title;
+    private String readyInMinutes;
+    private Integer servings;
+    private String image;
+    private String instructions;
+
+    public Recipe() {
+    }
+
+    public Recipe(String title, String readyInMinutes, Integer servings, String image, String instructions) {
+        this.title = title;
+        this.readyInMinutes = readyInMinutes;
+        this.servings = servings;
+        this.image = image;
+        this.instructions = instructions;
+    }
 }
